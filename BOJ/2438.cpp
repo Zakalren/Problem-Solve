44cde634cebf7ae65c16d6ed81481e67d962c395
Zakalren <bancreeper@gmail.com>
2020-11-15 21:56:39 +0900

    별 찍기 - 1

---
+++ BOJ/2438.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+main()
+{
+    int num;
+    scanf("%d", &num);
+    
+    for (int i = 1; i <= num; i++)
+    	std::cout << std::string(i, '*') << "\n";
+}
