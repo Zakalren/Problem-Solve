9ba5f21144155b8f94dd1cd8114306bfbd8e0753
Zakalren <bancreeper@gmail.com>
2020-11-15 21:50:53 +0900

    A+B - 3

---
+++ BOJ/10950.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+main()
+{
+    int num;
+    
+    scanf("%d", &num);
+    
+    for (int i = 0; i < num; i++)
+    {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a+b);
+    }
+}
