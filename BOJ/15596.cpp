9b5e499e748eb012d790be4688ce1e674105496c
Zakalren <bancreeper@gmail.com>
2020-11-16 15:07:00 +0900

    정수 N개의 합

---
+++ BOJ/15596.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+long long sum(std::vector<int> &a) 
+{
+    long long ans = 0;
+    
+    for (auto iter = a.begin(); iter != a.end(); iter++)
+        ans += *iter;
+    
+	return ans;
+}
