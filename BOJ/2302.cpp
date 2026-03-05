a734bf91a41b2f20787740323c345ce7943b382a
Zakalren <bancreeper@gmail.com>
2026-03-06 00:59:57 +0900

    Update

--- BOJ/2302.cpp
+++ BOJ/2302.cpp
@@ -2,30 +2,41 @@
 
 using namespace std;
 
-int dp[40];
-vector<int> f;
+bool vip[41];
+
+int dp[41];
+int ans = 1;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(nullptr)->sync_with_stdio(false);
 
     int N, M;
-
     cin >> N >> M;
-    while (M--) {
-        int tmp;
+
+    int tmp;
+    for (int i = 0; i < M; i++) {
         cin >> tmp;
-        f.push_back(tmp);
+        vip[tmp] = true;
     }
 
-    for (int i = 0; i < N; i++) {
-        if (i < 0 || i > N) continue;
+    dp[0] = 1;
+    dp[1] = 1;
+    for (int i = 2; i <= 40; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
 
-        if (find(f.begin(), f.end(), i + 1) != f.end()) {
+    int streak = 0;
+    for (int i = 1; i <= N; i++) {
+        if (vip[i]) {
+            ans *= dp[streak];
+            streak = 0;
         } else {
-            dp[i]++;
+            streak++;
         }
     }
+    ans *= dp[streak];
+
+    cout << ans << "\n";
 
     return 0;
 }
